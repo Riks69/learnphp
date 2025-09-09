9cd0380d661ee94f48c1363170d14cfe26091452
Riko-Arti Arju <riko-arti.arju@tptlive.ee>
2025-09-09 10:31:30 +0300

    Add if and

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
 <?php
+$num = 15;
+if($num > 10) {
+    var_dump('Suurem');
+} elseif ($num < 10){
 
-$bool = true && false;
-$bool = true || false;
-$bool = !false;
-$bool = (true && false || false) && !(true || false && true);
-$bool = 10 > 5;
-$bool = 10 < 5;
-$bool = 10 > 10;
-$bool = 10 < 10;
-$bool = 10 == 10;
-$bool = 10 >= 10;
-$bool = 10 <= 10;
-$bool = 10 == '10';
-$bool = 10 === '10';
-var_dump($bool);
+}  else {
+
+}
+
+$day = date('w', strtotime('09.09.2025'));
+var_dump($day);
+
+switch($day) {
+    case 1:
+        var_dump('Esmaspäev');
+        break;
+    case 2:
+        var_dump('Teisipäev');
+        break;
+    case 3:
+        var_dump('Kolmapäev');
+        break;
+    case 4:
+        var_dump('Neljapäev');
+        break;
+    case 5:
+        var_dump('Reede');
+        break;
+    case 6:
+        var_dump('Laupäev');
+        break;
+    case 7:
+        var_dump('Pühapäev');
+        break;
+    default:
+    var_dump('Imelikpäev')
+
+
+
+}
